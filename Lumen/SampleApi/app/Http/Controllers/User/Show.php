720299023d748d4e6c2a96d32d4fc3a97cb6720d
keishi0510fujii <keishifujii0510@gmail.com>
2021-02-09 21:18:27 +0900

    テスト用のユーザー情報取得コード実装
    factoryでユーザーの名前とIDを10行生成
    factoryでユーザーの能力値を生成
    生成した値をresponseでjson形式で返却

--- Lumen/SampleApi/app/Http/Controllers/User/Show.php
+++ Lumen/SampleApi/app/Http/Controllers/User/Show.php
@@ -3,20 +3,33 @@
 
 namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\UserAbility;
 
 
 class Show extends Controller
 {
     public function __invoke($id)
     {
-        return response()->json([
-            "name" => "Robin",
-            "level" => 99,
-            "hp" => 999,
-            "mp" => 99,
-            "attack_base" => 255,
-            "protect_base" => 255,
-            "speed" => 255
+        $users = factory(User::class, 10)->create();
+
+        $user = $users->where('id', '=', $id)->first();
+        if(is_null($user)){
+            return "No User";
+        }
+
+        $user_ability = factory(UserAbility::class)->create([
+            'user_id' => $id
         ]);
+
+        return response()->json([
+            'level' => $user_ability->level,
+            'hp' => $user_ability->hp,
+            'mp' => $user_ability->mp,
+            'attack' => $user_ability->attack,
+            'protect' => $user_ability->protect,
+            'speed' => $user_ability->speed,
+        ]);
+
     }
 }
