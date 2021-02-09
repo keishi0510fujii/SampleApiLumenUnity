3e90db67d54959106de8e9b71f5a610ab1bf4a64
keishi0510fujii <keishifujii0510@gmail.com>
2021-02-09 20:22:51 +0900

    デフォルトで作成されたUserモデルを変更
    nameしかみない用に変更

--- Lumen/SampleApi/app/User.php
+++ Lumen/SampleApi/app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'name',
     ];
 
     /**
@@ -27,6 +27,5 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $hidden = [
-        'password',
     ];
 }
