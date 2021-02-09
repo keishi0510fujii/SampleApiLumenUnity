52ae2867f9c6331330914ff5623d69ade4bf3e92
keishi0510fujii <keishifujii0510@gmail.com>
2021-02-09 20:18:50 +0900

    User情報のテーブル用migrationファイルを作成
    database/migrations/2021_02_09_000016_create_users_table.phpを新規作成
    migrations/2021_02_09_102549_create_user_abilities_table.phpを新規作成
    user_ablilitiesテーブルのuser_idカラムを外部キーとして設定(users_tableのid)

---
+++ Lumen/SampleApi/database/migrations/2021_02_09_102549_create_user_abilities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAbilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_abilities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('level')->default(1);
+            $table->unsignedInteger('hp')->default(25);
+            $table->unsignedInteger('mp')->default(0);
+            $table->unsignedInteger('attack')->default(15);
+            $table->unsignedInteger('protect')->default(15);
+            $table->unsignedInteger('speed')->default(15);
+            $table->unsignedBigInteger('user_id');
+
+            // 外部キー制約
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_abilities');
+    }
+}
