4a605ab8265897893eb9b93f8b67322af9cb4c8a
keishi0510fujii <keishifujii0510@gmail.com>
2021-02-09 20:29:46 +0900

    UserAbilityFactoryの不要部分を除去
    参照用にコメントしていた部分を全て削除

--- Lumen/SampleApi/database/factories/UserAbilityFactory.php
+++ Lumen/SampleApi/database/factories/UserAbilityFactory.php
@@ -10,13 +10,3 @@ $factory->define(App\UserAbility::class, function (Faker\Generator $faker) {
         'speed' => $faker->numberBetween(15,255),
     ];
 });
-
-
-//$table->bigIncrements('id');
-//$table->unsignedInteger('level')->default(1);
-//$table->unsignedInteger('hp')->default(25);
-//$table->unsignedInteger('mp')->default(0);
-//$table->unsignedInteger('attack')->default(15);
-//$table->unsignedInteger('protect')->default(15);
-//$table->unsignedInteger('speed')->default(15);
-//$table->unsignedBigInteger('user_id');
